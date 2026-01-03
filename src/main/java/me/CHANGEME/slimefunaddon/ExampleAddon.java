ecb28c5e9466364634bb67b6004e9fffde9d515a
OmegaApproximasterEthereal2004 <sunnybunnies09@naver.com>
2026-01-04 01:35:37 +0900

    Revise version support messages in ExampleAddon
    Updated console messages to clarify version support and added Korean translations.

--- src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
+++ src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
@@ -25,9 +25,10 @@ public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
             Component message = mm.deserialize( "<red>A</red><gold>p</gold><yellow>p</yellow><green>r</green><aqua>o</aqua><blue>x</blue><light_purple>i</light_purple>" + "<#aa55ff>m</#aa55ff><#ff5580>a</#ff5580><#ffcc55>s</#ffcc55><#55ffaa>t</#55ffaa><#55aaff>e</#55aaff><#aa55ff>r</#aa55ff> " + "<gradient:#55ffff:#5555ff>Studios 2004</gradient>" ); 
             // Example: send to console or player 
             System.out.println(message);
-            System.out.println("on 2025.12.23, we decided to not work on version that is new than 1.21.11. only 1.21.11 is supported. please use ViaVersion to allow 1.21.11+ from joining.");
+            System.out.println("on 2025.12.23, we decided to not work on version that is new than 1.21.11. only 1.21.11 is supported. please use ViaVersion to allow 1.21.11+ from joining!");
             System.out.println("For More Information, Please see https://netgames.approximasterstudios2004.ddnsgeek.com/v26andabove.html for more info.");
-            System.out.println("Rest In Peace Our Plugin Development 2022.5~2026.1.1");
+            System.out.println("2025년 12월 23일부로 저희 플러그인은 1.21.11만 지원합니다. 1.21.11보다 높은 버전은 지원하지 않습니다. 26.1 버전도 접속시키려면 ViaVersion을 사용해야 합니다!");
+            System.out.println("자세한 정보가 필요하시면 https://netgames.approximasterstudios2004.ddnsgeek.com/v26andabove.html을 확인하십시오.");
         }
 
         /*
